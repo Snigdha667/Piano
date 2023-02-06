c298c7542c1143bd9b45c0640ecc8680f8b8ccf3
Snigdha Singh <90649383+Snigdha667@users.noreply.github.com>
2023-02-07 00:49:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+// window.addEventListener('click',(e)=>{
+    // console.log(e);
+    const divi = document.querySelectorAll(".key");
+    // const divs = document.getElementsByTagName("div");
+
+
+divi[0].addEventListener('click',()=>{
+    var audio = document.getElementById("1");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[1].addEventListener('click',()=>{
+    var audio = document.getElementById("2");
+    audio.currentTime = 0;
+    audio.play();
+})
+divi[2].addEventListener('click',()=>{
+    var audio = document.getElementById("3");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[3].addEventListener('click',()=>{
+    var audio = document.getElementById("4");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[4].addEventListener('click',()=>{
+    var audio = document.getElementById("5");
+    audio.currentTime = 0;
+    audio.play();
+})
+divi[5].addEventListener('click',()=>{
+    var audio = document.getElementById("6");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[6].addEventListener('click',()=>{
+    var audio = document.getElementById("7");
+    audio.currentTime = 0;
+    audio.play();
+})
+divi[7].addEventListener('click',()=>{
+    var audio = document.getElementById("8");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[8].addEventListener('click',()=>{
+    var audio = document.getElementById("9");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[9].addEventListener('click',()=>{
+    var audio = document.getElementById("10");
+    audio.currentTime = 0;
+    audio.play();
+})
+divi[10].addEventListener('click',()=>{
+    var audio = document.getElementById("11");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[11].addEventListener('click',()=>{
+    var audio = document.getElementById("12");
+    audio.currentTime = 0;
+    audio.play();
+})
+divi[12].addEventListener('click',()=>{
+    var audio = document.getElementById("13");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+divi[13].addEventListener('click',()=>{
+    var audio = document.getElementById("14");
+    audio.currentTime = 0;
+    audio.play();
+
+})
+
+
+
+    
+// })
